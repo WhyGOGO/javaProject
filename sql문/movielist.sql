8e8e548cd69a9e61328ed88dec854a89a89eeb8f
WhyGOGO <minala7870@gmail.com>
2021-11-28 17:54:24 +0900

    sql문 초기화

--- sql문/movielist.sql
+++ sql문/movielist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- 생성 시간: 21-11-27 20:30
+-- 생성 시간: 21-11-28 17:52
 -- 서버 버전: 8.0.25
 -- PHP 버전: 8.0.6
 
@@ -37,22 +37,8 @@ CREATE TABLE `movie` (
   `score` float(3,2) NOT NULL COMMENT '평점(총10점)',
   `date` date NOT NULL COMMENT '개봉날짜',
   `rate` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '상영등급',
-  `image` mediumblob NOT NULL COMMENT '영화포스터'
+  `image` mediumblob COMMENT '영화포스터'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='영화리스트테이블';
-
---
--- 테이블의 덤프 데이터 `movie`
---
-
-INSERT INTO `movie` (`movie_id`, `title`, `director`, `summary`, `time`, `performer`, `score`, `date`, `rate`, `image`) VALUES
-(7, '박영민', '박영민', '박영민', 32, '박영민', 3.21, '0020-03-01', '13세', ''),
-(8, '박영민', '박영민', '박영민', 32, '박영민', 2.30, '2018-09-07', '15세', ''),
-(9, 'a', 'a', 'a', 32, 'a', 2.00, '2019-01-02', '32', ''),
-(10, 'a', 'a', 'a', 32, 'a', 2.00, '0020-02-08', '13', ''),
-(11, 'aa', 'aa', 'aa', 32, 'aa', 2.00, '2018-11-11', '32', ''),
-(12, '박영민', '박영민', '박영민', 32, '박영민', 2.00, '2018-11-11', '12', ''),
-(13, 'test', 'test', 'test', 11, 'test', 2.00, '2018-11-11', '11', ''),
-(14, 'test', 'test', 'test', 11, 'test', 2.00, '2018-11-11', '11', '');
 
 --
 -- 덤프된 테이블의 인덱스
@@ -72,7 +58,7 @@ ALTER TABLE `movie`
 -- 테이블의 AUTO_INCREMENT `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `movie_id` int NOT NULL AUTO_INCREMENT COMMENT '영화번호', AUTO_INCREMENT=15;
+  MODIFY `movie_id` int NOT NULL AUTO_INCREMENT COMMENT '영화번호', AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
